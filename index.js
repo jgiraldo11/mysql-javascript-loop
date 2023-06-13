ec66d6a4e4cad61259bfc1b268a8398003588b46
Jeniffer Giraldo <tris@Jeniffers-MacBook-Air-2.local>
2023-06-13 16:47:00 -0400

    FIRST commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import mysql from "mysql2";
+import { dbCredentials } from "./secrets.js";
+
+const db = mysql.createConnection(dbCredentials);
+
+const query = "SELECT * FROM Movies_table";
+
+db.query(
+    query,
+    function(err, results) {
+        console.log(err);
+        console.table(results);
+
+        for (let i=0; i<results.length; i++) {
+            console.log(results[i]);
+        }
+
+        db.end();
+    }
+)
+
